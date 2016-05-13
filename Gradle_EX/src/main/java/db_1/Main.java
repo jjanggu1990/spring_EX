2efaa2409aeaeb3828dc762e62e5fa384df5a6bc
LeeJinOh <jjanggu1990@naver.com>
2016-05-13 14:59:02 +0900

    db 연동문제 수정

--- Gradle_EX/src/main/java/db_1/Main.java
+++ Gradle_EX/src/main/java/db_1/Main.java
@@ -8,14 +8,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext context =
-				new ClassPathXmlApplicationContext("db_Ex.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("db_Ex.xml");
 
 		jdbcDao dao = context.getBean("jdbcDao",jdbcDao.class);
 		List<User> mem = dao.selectAll();
 		
 		for(User user : mem){
-			System.out.println(user);
+			System.out.print(user.getEmail()+", ");
+			System.out.print(user.getPassword()+", ");
+			System.out.print(user.getName()+", ");
+			System.out.print(user.getEmail()+"\n");
 		}
 		System.out.println("END!!");
 		
